6fb97e3ff9dcb142048846f237b4bc58a2e29166
sykez12 <adam_paddy@hotmail.co.uk>
2020-11-16 11:43:38 +0000

    Make a function for each colour

--- js/helper.js
+++ js/helper.js
@@ -7,3 +7,23 @@ function playNoise(soundElementId) {
     }
     noise = true;
 }
+
+function one() {
+    playNoise("sound1");
+    green.style.backgroundColor = "lightgreen";
+}
+
+function two() {
+    playNoise("sound2");
+    red.style.backgroundColor = "tomato";
+}
+
+function three() {
+    playNoise("sound3");
+    yellow.style.backgroundColor = "yellow";
+}
+
+function four() {
+    playNoise("sound4");
+    blue.style.backgroundColor = "lightskyblue";
+}
