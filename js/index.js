33033015a626a6b52f39575dd875de609aed80ec
sykez12 <adam_paddy@hotmail.co.uk>
2020-11-16 11:37:02 +0000

    Create gameturn function

--- js/index.js
+++ js/index.js
@@ -18,3 +18,25 @@ function play() {
 
     intervalId = setInterval(gameTurn, 800); //Runs gameturn function after set amount of time
 }
+
+function gameTurn() {
+    playerTurn = false; // Player cannot interact while on=false
+
+    if (flash == turn) { // If number of flashes=turncounter number then compturn is over
+        clearInterval(intervalId);
+        compTurn = false;
+        clearColor();
+        playerTurn = true; //Now player can interact with the game
+    }
+
+    if (compTurn) {
+        clearColor();
+        setTimeout(() => { //Performs this once after set amount of time
+            if (order[flash] == 1) one();
+            if (order[flash] == 2) two();
+            if (order[flash] == 3) three();
+            if (order[flash] == 4) four();
+            flash++;
+        }, 200)
+    }
+}
